20ea1aad1ed0fc03694abd98c6b743f481dc0c87
Rozi19 <rozaalene@gmail.com>
2026-06-03 08:22:44 -0700

    fix cors for production

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ const app = express();
 connectDB();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+   origin: [
+    "http://localhost:5173",
+    "https://trade-ethiopia-task-manager-fornten.vercel.app"
+  ],
   credentials: true
 }));
 
